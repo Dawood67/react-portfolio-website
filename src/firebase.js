776906f01b33653312f242c13dc482046013ffee
0xABDee <nazakalak70@gmail.com>
2022-11-28 18:16:10 +0500

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,14 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-};
+    apiKey: "AIzaSyCkMADtITvOGm_S_vyDQh88SlXHe1CUe-c",
+    authDomain: "react-portfolio-website-7d58d.firebaseapp.com",
+    projectId: "react-portfolio-website-7d58d",
+    storageBucket: "react-portfolio-website-7d58d.appspot.com",
+    messagingSenderId: "788299655934",
+    appId: "1:788299655934:web:173fc38c9f9d8770a193af",
+    measurementId: "G-KDD5MQKK0T"
+  };
 
 
 const app = initializeApp(firebaseConfig);
